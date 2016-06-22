0c45193e6b6d338ee3dc143cde5742c7a691c217
akitsu-sanae <akitsu.sanae@gmail.com>
2016-06-22 09:18:21 +0900

    add copyright notation

--- src/keyboard_.rs
+++ src/keyboard_.rs
@@ -1,3 +1,9 @@
+/*============================================================================
+  Copyright (C) 2016 akitsu sanae
+  https://github.com/akitsu-sanae/sakana
+  Distributed under the Boost Software License, Version 1.0. (See accompanying
+  file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+============================================================================*/
 
 pub struct Keyboard {
     pub is_left: bool,
